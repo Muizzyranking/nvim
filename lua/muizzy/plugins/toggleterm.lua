a40ba1e3c0ced38da553cef9081a3287c7d6adc0
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2023-02-21 20:17:55 +0100

    My NeoVim Config

---
+++ lua/muizzy/plugins/toggleterm.lua
@@ -0,0 +1,22 @@
+local status_ok, toggleterm = pcall(require, "toggleterm")
+if not status_ok then
+	return
+end
+
+toggleterm.setup({
+	size = 20,
+	open_mapping = [[<c-f>]],
+	direction = "float",
+	hide_numbers = true,
+	shade_filetype = {},
+	shade_terminals = true,
+	shading_factor = 2,
+	float_opts = {
+		border = "curved",
+		winblend = 0,
+		highlights = {
+			border = "Normal",
+			background = "Normal",
+		},
+	},
+})
